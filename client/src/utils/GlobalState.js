289713d60c2cb9ee7855bb2b7531c7617f164ace
Sebastian F <sebpfave2@gmail.com>
2021-08-16 21:02:58 -0400

    Refactor for use with redux

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,32 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from 'react'; //, { createContext, useContext }
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// Global State with Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// import { useProductReducer } from './reducers';
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
 
-export { StoreProvider, useStoreContext };
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
+
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
+
+// export { StoreProvider, useStoreContext };
+
+export default function StoreProvider(props) {
+  return <Provider store={store} {...props} />;
+}
